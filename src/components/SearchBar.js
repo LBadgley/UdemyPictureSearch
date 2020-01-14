4884b2fc2c14594c9b8e5876bf9e428bb30fc13d
Laura Badgley <l.badgleyy@gmail.com>
2020-01-14 12:10:14 -0800

    fixed state issue with search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 // class based component because we need to use STATE eventually
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
+  state = { term: '' };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    
+    console.log(this.state.term);
   }
+
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form"> 
+        <form onSubmit={this.onFormSubmit} className="ui form"> 
           <label>Image Search</label>
           {/* onInputChange is a callback function. We leave off the parens so we don't call the function when we don't want it to be called. Never add parens when passing the function to the event. */}
-          <input type="text" onChange={this.onInputChange} />
+          <input type="text" value={this.state.term} onChange={e => this.setState({ term: e.target.value })} />
         </form>
       </div>
     );
