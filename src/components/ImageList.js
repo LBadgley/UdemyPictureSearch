7346cda56028b57f88b6cbc0aa1206e4f8ec4073
Laura Badgley <l.badgleyy@gmail.com>
2020-01-14 15:34:28 -0800

    styling the image list

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,12 @@
+import './ImageList.css';
 import React from 'react';
-
 // functional component
 // assign key to root key (if there was a div around each image you'd put it in the div)
 const ImageList = props => {
   const images = props.images.map(({ description, id, urls }) => {
     return <img key={id} src={urls.regular} alt={description || 'Apologies, description text currently unavailable'} />
   })
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
