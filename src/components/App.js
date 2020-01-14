83dd6d3fb05ac9d0f9cd89ddb800a4a1729cfcaf
Laura Badgley <l.badgleyy@gmail.com>
2020-01-14 14:54:32 -0800

    cleaning up App component without axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 // place async in front of the method name and the await in front of the request
 // functional component returns Searchbar component
 class App extends React.Component {
-  async onSearchSubmit(term) {
-    const response = await axios
-    .get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID 17400cfee6736fe0dded86f931f6225447e7d0a8daa0778e090dfc0bc1d171a0"
-      }
+  state = {
+    images: [],
+  };
+
+  onSearchSubmit =  async(term) => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term }
     });
 
-    console.log(response.data.results);
+    this.setState({ images: response.data.results });
   } 
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images!
       </div>
     );
   }
