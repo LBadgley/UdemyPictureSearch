2bb50c01e3c818473223a6e47b59eda03f8c1cec
Laura Badgley <l.badgleyy@gmail.com>
2020-01-14 13:08:31 -0800

    added axios for fetching from the unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 // functional component returns Searchbar component
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    
   } 
 
   render() {
