07468d82cd5f028e31b448ff391363788a9a4f4a
Laura Badgley <l.badgleyy@gmail.com>
2020-01-14 13:39:07 -0800

    handling async/await for unsplash images

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 
+// place async in front of the method name and the await in front of the request
 // functional component returns Searchbar component
 class App extends React.Component {
-  onSearchSubmit(term) {
-    
+  async onSearchSubmit(term) {
+    const response = await axios
+    .get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 17400cfee6736fe0dded86f931f6225447e7d0a8daa0778e090dfc0bc1d171a0"
+      }
+    });
+
+    console.log(response.data.results);
   } 
 
   render() {
